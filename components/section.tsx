c6d6dbc9109a36f4aa9e2e654f72271b341ba2a2
Wei Lee <leewei48750@gmail.com>
2019-11-01 16:20:23 +0800

    feat(components/section.tsx): feat section

---
+++ components/section.tsx
@@ -0,0 +1,18 @@
+import React, { PropsWithChildren } from 'react';
+
+const Section: React.FC<{}> = (props: PropsWithChildren<{}>) => {
+  return (
+    <section>
+      {props.children}
+      <style jsx>{`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}</style>
+    </section>
+  );
+};
+
+export default Section;
