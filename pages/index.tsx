0a74e094b94f05c8797e6d5af37470fa7c9e6130
wiasliaw77210 <leewei48750@gmail.com>
2019-11-06 00:57:11 +0800

    refactor(pages/index.tsx): refactor by form component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 
 import Nav from '../components/nav';
 import Section from '../components/section';
 import Form from '../components/form';
+import { IndexState } from '../constant';
 
-const Index: React.FC = () => (
-  <main>
-    <Head>
-      <title>Home</title>
-    </Head>
-    <Nav />
-    <Section>
-      <Form />
-    </Section>
-  </main>
-);
+const Index: React.FC = () => {
+  const [currentState, setCurrent] = useState<IndexState>(IndexState.INPUT);
+  return (
+    <main>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Nav />
+      <Section>
+        <Form status={currentState} dispatch={setCurrent} />
+      </Section>
+    </main>
+  );
+};
 
 export default Index;
