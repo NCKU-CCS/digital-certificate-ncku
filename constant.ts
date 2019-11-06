9814888f10635bcd94cb24db0208492ecc931f9c
wiasliaw77210 <leewei48750@gmail.com>
2019-11-06 20:47:02 +0800

    feat(constant.tsx): add enum of states and data type

--- constant.ts
+++ constant.ts
@@ -1,11 +1,25 @@
 export enum IndexState {
-  INPUT,
-  READY,
-  SUCCESS,
-  FAILURE,
+  INPUT = 0,
+  READY = 1,
+  SUCCESS = 2,
+  FAILURE = 3,
 }
 export enum QueryState {
-  INPUT,
-  SUCCESS,
-  FAILURE,
+  INPUT = 10,
+  SUCCESS = 11,
+  FAILURE = 12,
+}
+export enum ReissueState {
+  INPUT = 20,
+  READY = 21,
+  SUCCESS = 22,
+  FAILURE = 23,
+}
+
+export interface IStudent {
+  name: string;
+  birth: string;
+  major: string;
+  other: string;
+  apply: string;
 }
