aca9c90e56ad75642562fde8bf86bf5d4e6b5ac8
wiasliaw77210 <leewei48750@gmail.com>
2019-11-06 20:51:17 +0800

    feat(index.tsx): compose page and logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,13 @@ import Head from 'next/head';
 import Nav from '../components/nav';
 import Section from '../components/section';
 import Form from '../components/form';
-import { IndexState } from '../constant';
+import IndexReady from '../components/indexReady';
+import IndexFinal from '../components/indexFinal';
+import { IndexState, IStudent } from '../constant';
 
 const Index: React.FC = () => {
   const [currentState, setCurrent] = useState<IndexState>(IndexState.INPUT);
+  const [user, setUser] = useState<IStudent>();
   return (
     <main>
       <Head>
@@ -15,7 +18,21 @@ const Index: React.FC = () => {
       </Head>
       <Nav />
       <Section>
-        <Form status={currentState} dispatch={setCurrent} />
+        {currentState === IndexState.INPUT ? (
+          <Form status={currentState} dispatch={setCurrent} setUser={setUser} />
+        ) : currentState === IndexState.READY ? (
+          <IndexReady
+            status={currentState}
+            setCurrent={setCurrent}
+            data={user}
+          />
+        ) : (
+          <IndexFinal
+            status={currentState}
+            setCurrent={setCurrent}
+            data={user}
+          />
+        )}
       </Section>
     </main>
   );
