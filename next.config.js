1bf880529690a6d9c489033090055f16d9e4da1b
TseHang <a54383813@gmail.com>
2019-11-07 14:57:26 +0800

    fix(github-path): add assertPrifix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const PROJ_NAME = 'digital-certificate-ncku';
+const GITHUB = process.env.DEPLOY_ENV === 'github';
+
 module.exports = {
+  assetPrefix: GITHUB ? `/${PROJ_NAME}/` : '',
   exportPathMap: function() {
     return {
       '/': { page: '/' },
