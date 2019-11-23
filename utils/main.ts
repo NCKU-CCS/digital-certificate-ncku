a17edb380673ccc1b37f2fe8029e44be553508aa
Wei Lee <leewei48750@gmail.com>
2019-11-23 19:52:59 +0800

    docs(main.ts): add api

--- utils/main.ts
+++ utils/main.ts
@@ -1,6 +1,14 @@
 import { mainInstance, nckuInstance } from './config';
 import runtimeEnv from '../runtimeEnv';
 
+/**
+ * @global
+ *  @host MAIN_HOST
+ *  @method GET
+ * Get from MAIN_HOST/query
+ * @function queryApi()
+ * @param {string} studentID
+ */
 const queryApi = async (studentID: string) => {
   const data = await mainInstance
     .get('/students/query/', {
@@ -11,6 +19,14 @@ const queryApi = async (studentID: string) => {
   return data;
 };
 
+/**
+ * @global
+ *  @host MAIN_HOST
+ *  @method GET
+ * Get from MAIN_HOST/students/gradinfo/
+ * @function gradinfoApi()
+ * @param {string} studentID
+ */
 const gradinfoApi = async (studentID: string) => {
   const data = await mainInstance
     .get('/students/gradinfo/', {
@@ -21,6 +37,14 @@ const gradinfoApi = async (studentID: string) => {
   return data.student_id ? data : null;
 };
 
+/**
+ * @global
+ *  @host MAIN_HOST
+ *  @method GET
+ * Get from MAIN_HOST/students/issue/
+ * @function issueApi()
+ * @param {string} studentID
+ */
 const issueApi = async (studentID: string) => {
   const data = await mainInstance
     .get('/students/issue/', {
@@ -31,6 +55,14 @@ const issueApi = async (studentID: string) => {
   return data;
 };
 
+/**
+ * @global
+ *  @host MAIN_HOST
+ *  @method GET
+ * Get from MAIN_HOST/students/reissue/
+ * @function reissueApi()
+ * @param {string} studentID
+ */
 const reissueApi = async (studentID: string) => {
   const data = await mainInstance
     .get('/students/reissue/', {
@@ -41,6 +73,15 @@ const reissueApi = async (studentID: string) => {
   return data;
 };
 
+/**
+ * @global
+ *  @host MAIN_HOST
+ *  @method GET
+ * Translate cardId into studentId from NCKU_HOST/ncku/queryno.php.
+ * If it is not necessary, this function can be deleted
+ * @function cardToStudentID()
+ * @param {string} studentID
+ */
 const cardToStudentID = async (cardID: string) => {
   const re = /[A-Z]{1}[0-9]{8}/i;
   let studentID: string = cardID;
@@ -57,6 +98,11 @@ const cardToStudentID = async (cardID: string) => {
   return studentID;
 };
 
+/**
+ * @function getRelativePath()
+ * @param {string} path
+ * resolve path for assets and links
+ */
 function getRelativePath(path: string): string {
   if (runtimeEnv.GITHUB) {
     const valid = /[^\/].*/g.exec(path);
